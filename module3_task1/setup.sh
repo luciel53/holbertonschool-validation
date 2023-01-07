9894c462509df952279e1a66230fa836ec9ad0e1
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 <l.leroty@gmail.com>
2023-01-07 15:39:53 +0100

    update setup hugo

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 apt-get update && apt-get install -y make golang-go git curl
-wget https://github.com/gohugoio/hugo/releases/download/v0.50/hugo_0.50_Linux-64bit.deb
+wget https://github.com/gohugoio/hugo/releases/download/v0.50/hugo_0.50_Linux-64bit.deb 
 sudo dpkg -i hugo_0.50_Linux-64bit.deb
-sudo rm hugo_0.30.2_Linux-64bit.deb
 make build
