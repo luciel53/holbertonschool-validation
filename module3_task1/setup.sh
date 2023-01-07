80a7f8d095580d150089d2ab8729cd1b3a30f82b
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 <l.leroty@gmail.com>
2023-01-07 15:58:05 +0100

    update setup delete build

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update && apt-get install -y make golang-go git curl
+sudo apt-get update && apt-get install -y make golang-go git curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugoversion.deb
 apt install ./hugoversion.deb
 
