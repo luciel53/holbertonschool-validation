2515007c03ead57b0c2c4e2df09698bb36953066
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 <l.leroty@gmail.com>
2023-01-07 16:02:58 +0100

    update setup sudo apt

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update && apt-get install -y make golang-go git curl
+apt-get update && apt-get install -y make golang-go git curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugoversion.deb
 sudo apt install ./hugoversion.deb
 
