be3cef6f19bbe69d082db9ac5d123c0d87e759a0
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 <l.leroty@gmail.com>
2023-01-09 20:49:39 +0100

    update makefile + setup markdown

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -3,5 +3,5 @@ apt-get update && apt-get install -y make golang-go git curl
 curl -L install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugoversion.deb
 apt install ./hugoversion.deb
 snap install golangci-lint
-
+npm install markdownlint --save-dev
 
