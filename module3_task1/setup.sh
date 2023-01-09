fa8b67e3e038b61ce87c8e396d73827fc7bc121f
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 <l.leroty@gmail.com>
2023-01-09 15:06:55 +0100

    update makefile lint variablequote

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update && apt-get install -y make golang-go git curl
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugoversion.deb
+curl -L install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugoversion.deb
 apt install ./hugoversion.deb
 snap install golangci-lint
 
